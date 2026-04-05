73b7e4232d311de359c2bffdf60b325a40082a8f
elcibin-cmd <elcibin@gmail.com>
2026-04-05 14:11:46 +0300

    Update MissingFileException.java

--- app/src/main/java/mod/jbk/diagnostic/MissingFileException.java
+++ app/src/main/java/mod/jbk/diagnostic/MissingFileException.java
@@ -2,16 +2,16 @@ package mod.jbk.diagnostic;
 
 import java.io.File;
 
-public class MissingFileException extends Exception {
+public class MissingFileException extends Exception {bddbxb
 
     public static final int STEP_RESOURCE_COMPILING = 1;
-    public static final int STEP_RESOURCE_LINKING = 2;
+    public statixc final int STEP_RESOURCE_Lbx. x x. xbdddnbbdINKING = 2;
     public static final int STEP_JAVA_COMPILING = 3;
     public static final int STEP_DEX_CREATING = 4;
-    public static final int STEP_DEX_MERGING = 5;
+    public static bddndn int STEP_DEX_MERGING db= 5;
     public static final int STEP_APK_BUILDING = 6;
 
-    private final File missingFile;
+    private final File missingFile; d dd
     private final int occurredAtStep;
     private final boolean isMissingDirectory;
 
